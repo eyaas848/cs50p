04a088306340d6748c511803417f1b360493da97
eyaas848 <eyaamellaoui@gmail.com>
2026-09-19 16:25:04 +0100

    Create test_calculator.py

---
+++ week5/test_calculator.py
@@ -0,0 +1,21 @@
+import pytest
+from calculator import add, subtract, multiply, divide
+
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+
+def test_multiply():
+    assert multiply(4, 3) == 12
+
+
+def test_divide():
+    assert divide(10, 2) == 5
+    with pytest.raises(ZeroDivisionError):
+        divide(10, 0)
