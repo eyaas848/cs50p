f8b786969804474b83cad51b86e8842955ea2a0b
eyaas848 <eyaamellaoui@gmail.com>
2026-09-19 16:16:14 +0100

    Create grade_converter.py

---
+++ week1/grade_converter.py
@@ -0,0 +1,20 @@
+def main():
+    score = int(input("Score (0-100): "))
+    print(get_letter(score))
+
+
+def get_letter(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+if __name__ == "__main__":
+    main()
